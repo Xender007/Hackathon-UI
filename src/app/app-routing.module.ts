3f58b64b15ea0f5d0ec2c5e569934462570e5b5c
XeNDeR <suprotim6@gmail.com>
2025-07-01 03:50:10 +0530

    upload added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChatbotComponent } from './chatbot/chatbot.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { LoginUploadComponent } from './login-upload/login-upload.component';
 
 const routes: Routes = [
   { path: 'chatbot', component: ChatbotComponent },
   { path: 'login', component: AdminLoginComponent },
+  { path: 'file/upload', component: LoginUploadComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }  
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hackathon-ui-final';
+
+  constructor(public router: Router) {}
+  
+  isFileUploadRoute(): boolean {
+    return this.router.url === '/file/upload';
+  }
 }
